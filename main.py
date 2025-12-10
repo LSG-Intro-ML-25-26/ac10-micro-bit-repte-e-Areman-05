7f3b8cd8e73fd85f609dfb43878730eb1751f1d1
Pablo Arenas <pablo.arenas@gracia.lasalle.cat>
2025-12-10 13:45:29 +0100

    repte 7 completado, revisado y completado todo, el repte e en general esta completado

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
+x = 2
+y = 2
+modo = 0
 
+def grafico_temp():
+    while modo == 1:
+        led.plot_bar_graph(input.temperature(), 50)
+        basic.pause(500)
+
+def mover_gota():
+    global x, y
+    while modo == 2:
+        led.plot(x, y)
+        basic.pause(50)
+        led.unplot(x, y)
+
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+
+        if accX <= -150 and x > 0:
+            x = x - 1
+        elif accX >= 150 and x < 4:
+            x = x + 1
+
+        if accY <= -150 and y > 0:
+            y = y - 1
+        elif accY >= 150 and y < 4:
+            y = y + 1
+
+def on_button_pressed_a():
+    global modo
+    modo = 1
+    grafico_temp()
+
+def on_button_pressed_b():
+    global modo
+    modo = 2
+    mover_gota()
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,1 +1,45 @@
+let x = 2
+let y = 2
+let modo = 0
+function grafico_temp() {
+    while (modo == 1) {
+        led.plotBarGraph(input.temperature(), 50)
+        basic.pause(500)
+    }
+}
 
+function mover_gota() {
+    let accX: number;
+    let accY: number;
+    
+    while (modo == 2) {
+        led.plot(x, y)
+        basic.pause(50)
+        led.unplot(x, y)
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+        if (accX <= -150 && x > 0) {
+            x = x - 1
+        } else if (accX >= 150 && x < 4) {
+            x = x + 1
+        }
+        
+        if (accY <= -150 && y > 0) {
+            y = y - 1
+        } else if (accY >= 150 && y < 4) {
+            y = y + 1
+        }
+        
+    }
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    modo = 1
+    grafico_temp()
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    modo = 2
+    mover_gota()
+})
